1579f122bab360d8ae5e7c9ab02f139f61f815dc
puxa7 <umpik123@gmail.com>
2026-03-29 14:34:58 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { createServer, IncomingMessage, ServerResponse } from "http";
+
+
+const PORT = 20191;
+
+const server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200, {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Connection": "keep-alive",
+      "Cache-Control": "no-cache"
+    });
+    res.end("Hello World. Create by Puxa");
+  }
+);
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
